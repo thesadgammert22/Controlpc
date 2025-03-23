b81d97c992e99f48baf75efd34ce4cbaed6fb9c7
thesadgammer22 <130327533+thesadgammert22@users.noreply.github.com>
2025-03-22 21:56:56 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install Node.js dependencies
+npm install express ws body-parser http-proxy-middleware axios
+
+# Install Python dependencies
+pip install git+https://github.com/aiortc/aiortc.git
