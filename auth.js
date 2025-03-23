13da90e9a21c257939ec9e0c90df11df9bb53781
thesadgammer22 <130327533+thesadgammert22@users.noreply.github.com>
2025-03-22 23:00:16 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,13 +1,13 @@
 const bcrypt = require('bcrypt');
 
 const USERS = {
-    admin: bcrypt.hashSync('12345', 10) // Hash the password
+    admin: bcrypt.hashSync('12345', 10) // Hardcoded credentials: username 'admin', password '12345'
 };
 
 const authMiddleware = (req, res, next) => {
     const { username, password } = req.body;
     if (USERS[username] && bcrypt.compareSync(password, USERS[username])) {
-        next(); // Login successful
+        next(); // Proceed if credentials are valid
     } else {
         res.status(401).send('Unauthorized');
     }
