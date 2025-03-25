b383e6bfc3ccd9b59d6b065165306bca24548803
thesadgammer22 <130327533+thesadgammert22@users.noreply.github.com>
2025-03-25 19:00:41 +0000

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/', (req, res) => {
 
 
 // Start the server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
